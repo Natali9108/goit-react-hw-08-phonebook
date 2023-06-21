dd51d0c10302959fc401bc7f6d5da844f6bf0562
Natali <114168847+Natali9108@users.noreply.github.com>
2023-06-21 18:16:01 +0300

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,7 +3,6 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { logIn } from '../../redux/auth/authOperations';
-import { toast } from 'react-hot-toast';
 
 const SignupSchema = yup
   .object()
