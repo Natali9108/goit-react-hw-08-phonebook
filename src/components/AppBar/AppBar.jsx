8368b65ca2725cf034748e6b53fd947f4d7d5629
Natali <114168847+Natali9108@users.noreply.github.com>
2023-06-28 00:57:18 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -4,11 +4,12 @@ import { Box } from '@chakra-ui/react';
 import { headerStyles } from './AppBarStyles';
 
 export const AppBar = () => {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, isRefreshing } = useAuth();
   return (
     <Box as="header" sx={headerStyles}>
       <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      {isLoggedIn && <UserMenu />}
+      {!isLoggedIn && !isRefreshing && <AuthNav />}
     </Box>
   );
 };
